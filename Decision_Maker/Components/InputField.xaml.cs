3b540d60c8945c096660a79919d4610be1caaf39
Õnnela Link <146323564+onnelaa@users.noreply.github.com>
2026-03-18 17:52:42 +0200

    loginpage update

--- Decision_Maker/Components/InputField.xaml.cs
+++ Decision_Maker/Components/InputField.xaml.cs
@@ -16,8 +16,8 @@ public partial class InputField : ContentView
     public static readonly BindableProperty IconSourceProperty =
         BindableProperty.Create(nameof(IconSource), typeof(string), typeof(InputField), string.Empty);
 
-    public static readonly BindableProperty InputTextProperty =
-        BindableProperty.Create(nameof(InputText), typeof(string), typeof(InputField), string.Empty, BindingMode.TwoWay);
+    public static readonly BindableProperty TextProperty =
+        BindableProperty.Create(nameof(Text), typeof(string), typeof(InputField), string.Empty, BindingMode.TwoWay);
 
     public static readonly BindableProperty IsPasswordProperty =
         BindableProperty.Create(nameof(IsPassword), typeof(bool), typeof(InputField), false);
@@ -43,10 +43,10 @@ public partial class InputField : ContentView
         set => SetValue(IconSourceProperty, value);
     }
 
-    public string InputText
+    public string Text
     {
-        get => (string)GetValue(InputTextProperty);
-        set => SetValue(InputTextProperty, value);
+        get => (string)GetValue(TextProperty);
+        set => SetValue(TextProperty, value);
     }
 
     public bool IsPassword
