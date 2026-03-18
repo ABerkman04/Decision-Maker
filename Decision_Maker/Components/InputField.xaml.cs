83c80455ee9e6a04d1621e58e0b24075eb7dd595
Õnnela Link <146323564+onnelaa@users.noreply.github.com>
2026-03-18 12:52:24 +0200

    poolik

---
+++ Decision_Maker/Components/InputField.xaml.cs
@@ -0,0 +1,63 @@
+﻿namespace Decision_Maker.Components;
+
+public partial class InputField : ContentView
+{
+    public InputField()
+    {
+        InitializeComponent();
+    }
+
+    public static readonly BindableProperty LabelTextProperty =
+        BindableProperty.Create(nameof(LabelText), typeof(string), typeof(InputField), string.Empty);
+
+    public static readonly BindableProperty PlaceholderTextProperty =
+        BindableProperty.Create(nameof(PlaceholderText), typeof(string), typeof(InputField), string.Empty);
+
+    public static readonly BindableProperty IconSourceProperty =
+        BindableProperty.Create(nameof(IconSource), typeof(string), typeof(InputField), string.Empty);
+
+    public static readonly BindableProperty InputTextProperty =
+        BindableProperty.Create(nameof(InputText), typeof(string), typeof(InputField), string.Empty, BindingMode.TwoWay);
+
+    public static readonly BindableProperty IsPasswordProperty =
+        BindableProperty.Create(nameof(IsPassword), typeof(bool), typeof(InputField), false);
+
+    public static readonly BindableProperty InputKeyboardProperty =
+        BindableProperty.Create(nameof(InputKeyboard), typeof(Keyboard), typeof(InputField), Keyboard.Default);
+
+    public string LabelText
+    {
+        get => (string)GetValue(LabelTextProperty);
+        set => SetValue(LabelTextProperty, value);
+    }
+
+    public string PlaceholderText
+    {
+        get => (string)GetValue(PlaceholderTextProperty);
+        set => SetValue(PlaceholderTextProperty, value);
+    }
+
+    public string IconSource
+    {
+        get => (string)GetValue(IconSourceProperty);
+        set => SetValue(IconSourceProperty, value);
+    }
+
+    public string InputText
+    {
+        get => (string)GetValue(InputTextProperty);
+        set => SetValue(InputTextProperty, value);
+    }
+
+    public bool IsPassword
+    {
+        get => (bool)GetValue(IsPasswordProperty);
+        set => SetValue(IsPasswordProperty, value);
+    }
+
+    public Keyboard InputKeyboard
+    {
+        get => (Keyboard)GetValue(InputKeyboardProperty);
+        set => SetValue(InputKeyboardProperty, value);
+    }
+}
